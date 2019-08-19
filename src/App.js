8d4a42b0f313b3d4978406a3f0d342c97a449e88
Yongzhi Yang <yongzhi.yang@57blocks.io>
2019-08-19 14:16:49 +0800

    readme

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SimpleReactModal from 'simple-react-modal';
 
 function App() {
-  const defaultContainerStyles = {
-    padding: '10px 10px 30px',
-    borderRadius: '10px',
-    margin: '10vh auto 5px',
-    width: '90vw',
-    maxWidth: '500px',
-    minHeight: '470px',
-    backgroundColor: '#F9F9F9'
-  };
-
-  const modalStyle = {
-    transition: 'opacity 0.25s ease-in-out',
-    background: 'rgba(48, 48, 48, 0.85)',
-    fontFamily: null
-  };
-
-  const containerStyles = {
-    ...defaultContainerStyles
-  };
   return (
     <div className="App">
       <header className="App-header">
@@ -39,13 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <SimpleReactModal
-        show={true}
-        style={modalStyle}
-        containerStyle={containerStyles}
-      >
-        test
-      </SimpleReactModal>
+      <button>Open the loader</button>
     </div>
   );
 }
