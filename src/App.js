1a4d1bc4960c693103a93963b525353e1d83e57b
Yongzhi Yang <yongzhi.yang@57blocks.io>
2019-12-31 09:06:08 +0800

    update the text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Update this text and see it
         </a>
       </header>
     </div>
