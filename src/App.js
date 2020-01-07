c07a403bcf0d62d2e29c2938b458698614194da5
Yongzhi Yang <yongzhi.yang@57blocks.io>
2020-01-07 21:48:20 +0800

    update the circleci script

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Well, launching production now.</div>;
+  return (
+    <div className="App">
+      Well, launching {process.env.REACT_APP_ENV} now.
+      <br />
+      Call API through {process.env.REACT_APP_API_URL}
+    </div>
+  );
 }
 
 export default App;
